1a8c28b87c5b993a387ef390ea8104a772b60601
Your Name <dodepchai@gmail.com>
2023-12-23 15:19:41 +0800

    fix comment bug

--- ToDoListLaravel/tailwind.config.js
+++ ToDoListLaravel/tailwind.config.js
@@ -1,4 +1,4 @@
-// import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
